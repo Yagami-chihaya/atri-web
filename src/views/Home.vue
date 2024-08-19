b821660f98424934dfc6b8a9f57de1a1de1dc7c5
yagamichihaya <935760346@qq.com>
2024-08-19 17:28:57 +0800

    补充图床链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,14 +21,14 @@
         <div class="list_box">
           <ul class="cg_list" v-show="!CGStyle">
             <li v-for="item in cg_list[0].list" class="cg_info" :key="item" @click="showCG(item.url)">
-              <img :src="StringToURL(item.url).href" alt=" ">
+              <img :src="StringToURL(item.coverURL).href" alt=" ">
             </li>
             <el-pagination background layout="prev, pager, next" :total="1000" />
           </ul>
           <el-carousel v-show="CGStyle" class="cg_card" :interval="4000" type="card" height="300px">
             <el-carousel-item v-for="item in cg_list[0].list" :key="item" @click="showCG(item.url)">
               <div class="imgBox">
-                <img :src="StringToURL(item.url).href" alt=" ">
+                <img :src="StringToURL(item.coverURL).href" alt=" ">
               </div>
               
             </el-carousel-item>
@@ -69,6 +69,7 @@ export default {
           list: [
             {
               id: 0,
+              coverURL:"https://img.picgo.net/2024/08/09/ev000al7d5c8f298a09b64d.md.jpg",
               url: "https://img.picgo.net/2024/08/09/ev000al7d5c8f298a09b64d.jpg",
               children: [
 
@@ -76,6 +77,7 @@ export default {
             },
             {
               id: 1,
+              coverURL:"https://img.picgo.net/2024/08/09/ev001aldfb66c3d149bd6d5.md.png",
               url: "https://img.picgo.net/2024/08/09/ev001aldfb66c3d149bd6d5.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev001bl6420e9a6243f755a.png",
@@ -84,6 +86,7 @@ export default {
             },
             {
               id: 2,
+              coverURL:"https://img.picgo.net/2024/08/09/ev002al50e8b3a55193f75d.md.png",
               url: "https://img.picgo.net/2024/08/09/ev002al50e8b3a55193f75d.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev002bl2a7d63c10b9c3819.png",
@@ -91,6 +94,7 @@ export default {
             },
             {
               id: 3,
+              coverURL:"https://img.picgo.net/2024/08/09/ev003al81372ddfef4f8bfc.md.png",
               url: "https://img.picgo.net/2024/08/09/ev003al81372ddfef4f8bfc.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev003bl42ea1afd8b49f5aa.png",
@@ -103,6 +107,7 @@ export default {
             },
             {
               id: 4,
+              coverURL:"https://img.picgo.net/2024/08/09/ev004al1f51489f7b23db97.md.png",
               url: "https://img.picgo.net/2024/08/09/ev004al1f51489f7b23db97.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev004bldcd0d2f5a13aa5ed.png",
@@ -110,6 +115,7 @@ export default {
             },
             {
               id: 5,
+              coverURL:"https://img.picgo.net/2024/08/09/ev005al63d7fb212b65f517.md.png",
               url: "https://img.picgo.net/2024/08/09/ev005al63d7fb212b65f517.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev005bl3aafa2bf5a681213.png",
@@ -118,6 +124,7 @@ export default {
             },
             {
               id: 6,
+              coverURL:"https://img.picgo.net/2024/08/09/ev006alf256c7859e44f0be.md.png",
               url: "https://img.picgo.net/2024/08/09/ev006alf256c7859e44f0be.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev006bl91db0ede1ad7be0c.png",
@@ -125,6 +132,7 @@ export default {
             },
             {
               id: 7,
+              coverURL:"https://img.picgo.net/2024/08/09/ev007alfa9ec5c48286aada.md.png",
               url: "https://img.picgo.net/2024/08/09/ev007alfa9ec5c48286aada.png",
               children: [
                 "https://img.picgo.net/2024/08/09/ev007blfe640a0b7b432789.png",
@@ -132,11 +140,215 @@ export default {
             },
             {
               id: 8,
+              coverURL:"https://img.picgo.net/2024/08/09/ev008al5640fdc350781649.md.png",
               url: "https://img.picgo.net/2024/08/09/ev008al5640fdc350781649.png",
               children: [
-                "https://img.picgo.net/2024/08/09/ev008blede18c5ecbc3e8f6.png"
+                "https://img.picgo.net/2024/08/09/ev008blede18c5ecbc3e8f6.png",
+                "https://img.picgo.net/2024/08/09/ev008clf9584d32c2101ce5.png"
               ]
             },
+            {
+              id: 9,
+              coverURL:"https://img.picgo.net/2024/08/09/ev009al3790029e482a2be3.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev009al3790029e482a2be3.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev009bl41e4a04f7206245c.png",
+               
+              ]
+            },
+            {
+              id: 10,
+              coverURL:"https://img.picgo.net/2024/08/09/ev010ale6cc7265ee65c721.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev010ale6cc7265ee65c721.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev010blb2510e437110348f.png",
+           
+              ]
+            },
+            {
+              id: 11,
+              coverURL:"https://img.picgo.net/2024/08/09/ev011alf59aaea4eebcd998.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev011alf59aaea4eebcd998.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev011bl7d9c368f8c938c74.png",
+                "https://img.picgo.net/2024/08/09/ev011cl87ce67f06e027a5b.png"
+              ]
+            },
+            {
+              id: 12,
+              coverURL:"https://img.picgo.net/2024/08/09/ev012alfc03ed3e5a3a20e1.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev012alfc03ed3e5a3a20e1.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev012bl105dd9b9297304c8.png",
+                "https://img.picgo.net/2024/08/09/ev012claa938c2f4c7c7238.png"
+              ]
+            },
+            {
+              id: 13,
+              coverURL:"https://img.picgo.net/2024/08/09/ev013ald4e18c4444acd3ae.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev013ald4e18c4444acd3ae.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev013blaf15e6e2bf6d34df.png",
+           
+              ]
+            },
+            {
+              id: 14,
+              coverURL:"https://img.picgo.net/2024/08/09/ev014al36ff310ac269329c.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev014al36ff310ac269329c.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev014bla040b74df874ea7a.png",
+           
+              ]
+            },
+            {
+              id: 15,
+              coverURL:"https://img.picgo.net/2024/08/09/ev015ala29479487eb6fc6c.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev015ala29479487eb6fc6c.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev015bl3fdb4cb02ce9a2bb.png",
+                "https://img.picgo.net/2024/08/09/ev015cl64eb5d9d136ad07b.png"
+              ]
+            },
+            {
+              id: 16,
+              coverURL:"https://img.picgo.net/2024/08/09/ev016al7da9e96ad08ea9d8.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev016al7da9e96ad08ea9d8.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev016bl08c3a2a2d6d7f4b9.png",
+        
+              ]
+            },
+            {
+              id: 17,
+              coverURL:"https://img.picgo.net/2024/08/09/ev017ald5aeec49823e5840.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev017ald5aeec49823e5840.png",
+              children: [
+
+              ]
+            },
+            {
+              id: 18,
+              coverURL:"https://img.picgo.net/2024/08/09/ev018al4e997679f0590713.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev018al4e997679f0590713.png",
+              children: [
+
+              ]
+            },
+            {
+              id: 19,
+              coverURL:"https://img.picgo.net/2024/08/09/ev019al4a6a6a1552f2ccf0.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev019al4a6a6a1552f2ccf0.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev019bla8738f29d550d263.png",
+
+              ]
+            },
+            {
+              id: 20,
+              coverURL:"https://img.picgo.net/2024/08/09/ev020al4d18a90c84936bf4.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev020al4d18a90c84936bf4.png",
+              children: [
+              ]
+            },
+            {
+              id: 21,
+              coverURL:"https://img.picgo.net/2024/08/09/ev021ald6669b48a1aa7c02.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev021ald6669b48a1aa7c02.png",
+              children: [
+
+              ]
+            },
+            {
+              id: 22,
+              coverURL:"https://img.picgo.net/2024/08/09/ev101al4841996aae983cef.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev101al4841996aae983cef.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev101bl245826b7729a20bb.png"
+              ]
+            },
+            {
+              id: 23,
+              coverURL:"https://img.picgo.net/2024/08/09/ev102al5a3e9f55d1f76fe9.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev102al5a3e9f55d1f76fe9.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev102bl9b018715491caa4b.png"
+              ]
+            },
+            {
+              id: 24,
+              coverURL:"https://img.picgo.net/2024/08/09/ev103ala97b64a9f3f0ce3b.md.png",
+              url: "https://img.picgo.net/2024/08/09/ev103ala97b64a9f3f0ce3b.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/ev103bl8817ca26d158435a.png",
+                "https://img.picgo.net/2024/08/09/ev103cl1ee6a7ba60dfeb41.png"
+              ]
+            },
+            {
+              id: 25,
+              coverURL:"https://img.picgo.net/2024/08/09/sd001a26e3d8b827956df5.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd001a26e3d8b827956df5.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/sd001be0aed125e62df66d.png",
+                "https://img.picgo.net/2024/08/09/sd001cfbaa6f2b0c234315.png"
+              ]
+            },
+            {
+              id: 26,
+              coverURL:"https://img.picgo.net/2024/08/09/sd002ad060ecd447faf480.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd002ad060ecd447faf480.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/sd002bc97dadaddb1ecff9.png",
+               
+              ]
+            },
+            {
+              id: 27,
+              coverURL:"https://img.picgo.net/2024/08/09/sd003a0bf549ab062136c2.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd003a0bf549ab062136c2.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/sd003b9911de75b848fd14.png",
+                "https://img.picgo.net/2024/08/09/sd003c5907beb138e53a1e.png"
+              ]
+            },
+            {
+              id: 28,
+              coverURL:"https://img.picgo.net/2024/08/09/sd004a1ef7089b07c4864c.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd004a1ef7089b07c4864c.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/sd004bed6e71d10e016b6a.png",
+          
+              ]
+            },
+            {
+              id: 29,
+              coverURL:"https://img.picgo.net/2024/08/09/sd005a0825ed01c69c5f09.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd005a0825ed01c69c5f09.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/sd005b70c068cb975bcb3c.png",
+                "https://img.picgo.net/2024/08/09/sd005c5cfb180d15263505.png"
+              ]
+            },
+            {
+              id: 30,
+              coverURL:"https://img.picgo.net/2024/08/09/sd006a79c7aaf447aa0900.md.png",
+              url: "https://img.picgo.net/2024/08/09/sd006a79c7aaf447aa0900.png",
+              children: [
+
+          
+              ]
+            },
+            {
+              id: 31,
+              coverURL:"https://img.picgo.net/2024/08/09/title_base8c69345d0600c787.md.png",
+              url: "https://img.picgo.net/2024/08/09/title_base8c69345d0600c787.png",
+              children: [
+                "https://img.picgo.net/2024/08/09/title_base223d2708d7c4dc262.png",
+          
+              ]
+            },
+          
+            
           ]
 
 
